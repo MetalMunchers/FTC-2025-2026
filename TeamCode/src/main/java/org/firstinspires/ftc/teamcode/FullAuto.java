28b77f96525c0f5d18d98eec59d139310b103b00
Bevergames2018 <jaapdegroot999@gmail.com>
2026-01-22 00:29:25 +0100

    Initial commit for the onBot java code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullAuto.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="FullAuto", group="Linear OpMode")
+public class FullAuto extends LinearOpMode {
+    // Initialize motor variables
+    ElapsedTime runtime = new ElapsedTime();
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    DcMotor flywheelRight;
+    DcMotor flywheelLeft;
+
+    @Override
+    public void runOpMode() {
+        
+        //Assign hardware map to variables
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        
+        // Set the motor directions
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        if (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+                telemetry.update();
+
+            runtime.reset();
+
+            // run until the end of the match (driver presses STOP)
+            while (opModeIsActive() && runtime.seconds() <= 1) {
+                leftDrive.setPower(0.25);
+                rightDrive.setPower(0.25);
+            }
+
+            while (opModeIsActive() && runtime.seconds() > 1) {
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
+            }
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDrive.java
@@ -0,0 +1,140 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="ManualDrive", group="Linear OpMode")
+public class ManualDrive extends LinearOpMode {
+
+    // Initialize motor variables
+    Servo ballControl;
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    DcMotor flywheelRight;
+    DcMotor flywheelLeft;
+    DcMotor elastiekWiel;
+    DcMotor lift;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Assign hardware map to variables
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        flywheelRight = hardwareMap.get(DcMotor.class, "flywheel_right");
+        flywheelLeft = hardwareMap.get(DcMotor.class, "flywheel_left");
+        elastiekWiel = hardwareMap.get(DcMotor.class, "elastiek_wiel");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+
+        ballControl = hardwareMap.get(Servo.class, "ball_control");
+
+        
+        // Set the motor directions
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        flywheelLeft.setDirection(DcMotor.Direction.REVERSE);
+        flywheelRight.setDirection(DcMotor.Direction.FORWARD);
+        elastiekWiel.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotor.Direction.REVERSE);
+        
+        // Initialize variables
+        double Power;
+        double Direction;
+        boolean SlowMode = false;
+        double PowerL = 0;
+        double PowerR = 0;
+        
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            Power = -gamepad1.left_stick_y; // Y axis on sticks are inverted
+            Direction = gamepad1.right_stick_x;
+            
+            // SlowMode Control
+            if (gamepad1.right_trigger > 0.25) {
+                SlowMode = true;
+            } else {
+                SlowMode = false; 
+            }
+            
+            // Set motor power
+            PowerL = Power;
+            PowerR = Power;
+
+            // Apply turning direction
+            if (Direction > 0) {
+                if (Power > 0) {
+                    PowerL = PowerL - Direction;
+                } else if (Power < 0) {
+                    PowerL = PowerL + Direction;   
+                }
+            } else if (Direction < 0) {
+                if (Power > 0) {
+                    PowerR = PowerR + Direction;
+                } else if (Power < 0) {
+                    PowerR = PowerR - Direction;
+                }
+            }
+            
+            // Makes the bot able to turn while standing still
+            if (Power == 0 && Direction != 0){
+                PowerL = Direction * 0.66 * -1;
+                PowerR = Direction * 0.66;
+            }
+
+            // Apply SlowMode multiplier
+            if (SlowMode){
+                    PowerL = PowerL * 0.5;
+                    PowerR = PowerR * 0.5;
+            }
+
+            // Send calculated power to wheels
+            leftDrive.setPower(PowerL);
+            rightDrive.setPower(PowerR);
+            
+            //Flywheel Control
+            if (gamepad1.left_trigger > 0.5){
+                flywheelLeft.setPower(1);
+                flywheelRight.setPower(1);
+            } else {
+                flywheelLeft.setPower(0);
+                flywheelRight.setPower(0);
+            }
+
+            //if (gamepad1.b) {
+            //    ballControl.setPosition(0.3);
+            //} else {
+            //    ballControl.setPosition(0.10);
+            //}
+            
+            if (gamepad1.a) {
+                elastiekWiel.setPower(1);
+            } else {
+                elastiekWiel.setPower(0);
+            }
+            
+            if (gamepad1.b) {
+                lift.setPower(1);
+            } else {
+                lift.setPower(0);
+            }
+            
+            // Debug
+            telemetry.addData("PowerL", PowerL);
+            telemetry.addData("PowerR", PowerR);
+            telemetry.addData("Servo Position", ballControl.getPosition());
+            //telemetry.addData("LY", gamepad1.left_stick_y);
+            //telemetry.addData("LX", gamepad1.left_stick_x);
+            //telemetry.addData("RY", gamepad1.right_stick_y);
+            //telemetry.addData("RX", gamepad1.right_stick_x);
+        
+            telemetry.update();
+        }
+    }
+}
